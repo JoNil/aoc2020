5c739c425acf2a4dfeb2ab0d65e952ece52294d6
Jonathan Nilsson <jonathan@voysys.se>
2020-12-17 15:46:36 +0100

    Begun 16

---
+++ src/bin/16_a.rs
@@ -0,0 +1,39 @@
+use aoc2020::get_input;
+use itertools::Itertools;
+
+fn solve(input: &str) -> i32 {
+    0
+}
+
+fn main() {
+    let input = get_input();
+
+    let result = solve(&input);
+
+    println!("{:?}", result);
+}
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn test_solve() {
+        assert_eq!(
+            super::solve(
+                "class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12"
+            ),
+            71
+        );
+    }
+}
